9a6a23f4d9ff5833d4b7a434c35b60e9e95fa6ce
ssharma1307 <sharma.sukhdeep30@gmail.com>
2024-12-04 20:08:01 -0500

    Added Chat area

--- gemini-clone/src/App.jsx
+++ gemini-clone/src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
 
 const App = () => {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   )
 }

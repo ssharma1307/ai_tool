6bebef92e892d395ccafd3a38b02362a1b16a512
ssharma1307 <sharma.sukhdeep30@gmail.com>
2024-12-05 16:18:00 -0500

    added chatbox

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Main.css';
 import { assets } from '../../assets/assets';
@@ -31,6 +32,19 @@ const Main = () => {
                 <p>Improve the reliability of the following code</p>
                 <img src={assets.code_icon} alt=""/>
             </div>
+        </div>
+        <div className="main-bottom">
+            <div className="search-box">
+                <input type="text" placeholder='Enter a prompt here'/>
+                <div>
+                    <img src={assets.gallery_icon} alt=''/>
+                    <img src={assets.mic_icon} alt=''/>
+                    <img src={assets.send_icon} alt=''/>
+                </div>
+            </div>
+            <p className="bottom-info">
+                Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
+            </p>
         </div>
       </div>
     </div>
